077ea26cb72e4211ff507de5051e9e7036c15e2a
goldrunnercode <81481593+goldrunnercode@users.noreply.github.com>
2022-12-04 16:19:54 -0700

    Adding routing to page

--- WebApp/my-app/src/App.js
+++ WebApp/my-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import './App.css'
+import Header from '../src/Components/Header'
+import { Routes, Route } from 'react-router-dom'
+import Home from '../src/Pages/Home'
+import Products from '../src/Pages/Products'
+import Blog from '../src/Pages/Blog'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </>
+  )
 }
-
-export default App;
+export default App
