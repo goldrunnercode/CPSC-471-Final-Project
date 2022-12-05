a19b6bc61812a38b3d8e8c6f6dde554e52d2c749
goldrunnercode <81481593+goldrunnercode@users.noreply.github.com>
2022-12-04 18:05:41 -0700

    Changes to blog

--- WebApp/my-app/src/App.js
+++ WebApp/my-app/src/App.js
@@ -6,18 +6,18 @@ import { BrowserRouter as Router,
   Link } from 'react-router-dom';
 import Home from '../src/Pages/Home'
 import Products from '../src/Pages/Products'
-import Blog from '../src/Pages/Blog'
+import Orders from './Pages/Orders'
 function App() {
   return (
     <Router>
       <nav style = {{display:'flex', flexDirection:'column'}}>
         <Link to="/">Home</Link>
         <Link to="/products">Products</Link>
-        <Link to="/blog">Blog</Link>
+        <Link to="/orders">Orders</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
+        <Route path="/orders" element={<Orders/>} />
         <Route path="/products" element={<Products />} />
       </Routes>
     </Router>
