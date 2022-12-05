9b8b9d3cfff0f98da296bb60614eae8099f85fba
bryce219 <41980089+bryce219@users.noreply.github.com>
2022-12-04 17:20:59 -0700

    home stuff

--- WebApp/my-app/src/Pages/Home.js
+++ WebApp/my-app/src/Pages/Home.js
@@ -1,5 +1,27 @@
 import React from 'react'
+
+// Make a text input field, and store the value in the state, then display it once the user presses enter
 function Home() {
-  return <div>Home</div>
+  const [text, setText] = React.useState('')
+  const [display, setDisplay] = React.useState('')
+
+  const handleChange = (event) => {
+    setText(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setDisplay(text)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={handleChange} />
+        <button type="submit">Submit</button>
+      </form>
+      <p>{display}</p>
+    </div>
+  )
 }
 export default Home
