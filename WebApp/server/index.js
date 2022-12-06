b45db33aef3947d535bb6b4ed249fceb1dc4c8e2
bryce219 <41980089+bryce219@users.noreply.github.com>
2022-12-06 13:45:13 -0700

    fix

--- WebApp/server/index.js
+++ WebApp/server/index.js
@@ -10,21 +10,20 @@ var con = mysql.createConnection({
   database: "fullstore"
 });
 
+// only perform handshake once
 app.get('/:EmailAddress', (req, res) => {
+  if(con.state === 'disconnected') {
     con.connect(function(err) {
+      if (err) throw err;
+      con.query("SELECT * FROM Orders WHERE CustomerEmail = '"+req.params.EmailAddress +"'", function (err, result, fields) {
         if (err) throw err;
-        con.query("SELECT * FROM Orders WHERE CustomerEmail = '"+req.params.EmailAddress +"'", function (err, result, fields) {
-          if (err) throw err;
-          res.send(result)
-        });
+        res.send(result)
       });
-      if (con.state !== 'disconnected') {
-        con.end(function(err) {
-          if (err) throw err;
-          console.log('Connection closed');
-        });
-      }
-});
+    });
+  }
+})
+
+  
 
 
 app.listen(port, () => {
