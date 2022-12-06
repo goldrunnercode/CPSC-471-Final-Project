27226bb79ef4221d8106524ddb3eb533d787a95b
goldrunnercode <81481593+goldrunnercode@users.noreply.github.com>
2022-12-06 00:48:23 -0700

    Mysql file now with a select * from customer query

--- WebApp/server/mysql.js
+++ WebApp/server/mysql.js
@@ -3,11 +3,14 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "foo"
+  password: "password",
+  database: "fullstore"
 });
 
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
+  con.query("SELECT * FROM Customer", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
 });
-
